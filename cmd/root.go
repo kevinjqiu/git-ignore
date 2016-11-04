e2fd86d2cc68c913e4ee7a9422f9625cf5bc2602
Kevin Jing Qiu <kevin@idempotent.ca>
2016-11-04 00:45:26 -0400

    List available .gitignores

--- cmd/root.go
+++ cmd/root.go
@@ -5,15 +5,69 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"os/exec"
+	"log"
+	"bytes"
+	"os/user"
+	"strings"
 )
 
-var cfgFile string
+var language string
+var listLangs bool
+var shouldAppend bool
+const REMOVE_GITIGNORE_REPO_URL = "https://github.com/github/gitignore.git"
+
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func cloneGitIgnoreRepo(path string) {
+	cloneCommand := exec.Command("git", "clone", REMOVE_GITIGNORE_REPO_URL, path)
+	if err := cloneCommand.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getGitIgnoreFiles(path string) []string {
+	var out bytes.Buffer
+	lsCommand := exec.Command("ls", "-1", path)
+	lsCommand.Stdout = &out
+
+	if err := lsCommand.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	result := make([]string, 0)
+	for _, line := range(strings.Split(out.String(), "\n")) {
+		if strings.HasSuffix(line, ".gitignore") {
+			result = append(result, line)
+		}
+	}
+	return result
+}
 
 var RootCmd = &cobra.Command{
 	Use:   "git-ignore",
 	Short: "Setup .gitignore file",
-	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		currentUser, _ := user.Current()
+		var localGitignoreRepoPath = currentUser.HomeDir + "/.gitignore-repo"
+
+		if !pathExists(localGitignoreRepoPath) {
+			cloneGitIgnoreRepo(localGitignoreRepoPath)
+		}
+
+		out := getGitIgnoreFiles(localGitignoreRepoPath)
+		for _, line := range(out) {
+			if strings.HasSuffix(line, ".gitignore") {
+				fmt.Println(line)
+			}
+		}
+	},
 }
 
 func Execute() {
@@ -24,8 +78,7 @@ func Execute() {
 }
 
 func init() {
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.git-ignore.yaml)")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.PersistentFlags().StringVar(&language, "lang", "", "Generate language specific .gitignore file")
+	RootCmd.Flags().BoolVarP(&shouldAppend, "append", "a", false, "Append to the current .gitignore")
+	RootCmd.Flags().BoolVarP(&listLangs, "list", "", false, "List available language-specific .gitignore files")
 }
