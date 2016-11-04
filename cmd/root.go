09feee1da26c6a5fbc49f68e1fcc05b1878eb5a3
Kevin Jing Qiu <kevin@idempotent.ca>
2016-11-03 23:42:25 -0400

    Init

---
+++ cmd/root.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var RootCmd = &cobra.Command{
+	Use:   "git-ignore",
+	Short: "Setup .gitignore file",
+	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.git-ignore.yaml)")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/kevinjqiu/git-ignore/cmd"
+
+func main() {
+	cmd.Execute()
+}
